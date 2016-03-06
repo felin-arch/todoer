316436c171de61a1d1f3e95429f2ce5a68c2c79c
Daniel Szpisjak <felin.arch@gmail.com>
2016-03-06 20:01:54 +0100

    add simple criterion factory

--- app/criteria/factory.py
+++ app/criteria/factory.py
@@ -1,8 +1,40 @@
+from app.criteria.logical import *
+from app.criteria.item import *
+from app.criteria.project import *
 
-class CriteriaFactory():
+MAPPING = {
+    'true': TrueCriterion,
+    'false': FalseCriterion,
+
+    'item_has_due_date': ItemHasDueDateCriterion,
+
+    'project_name_equals': ProjectNameEqualsCriterion
+}
+
+class CriterionFactory():
 
     def __init__(self):
         pass
 
-    def create(self, criteria_definition):
-        pass
+    def create(self, definition):
+        criterion_type, arguments = self._unpack_definition(definition)
+        CriterionClass = self._get_criterion_class(criterion_type)
+
+        return CriterionClass()
+
+    def _unpack_definition(self, definition):
+        if len(definition.keys()) is not 1:
+            raise InvalidCriterionDefinitionError('Definition should only have a single key')
+
+        return definition.popitem()
+
+    def _get_criterion_class(self, criterion_type):
+        if criterion_type not in MAPPING:
+            raise InvalidCriterionDefinitionError(
+                'No such criterion: {0}'.format(criterion_type))
+
+        return MAPPING[criterion_type]
+
+
+class InvalidCriterionDefinitionError(RuntimeError):
+    pass
--- test/criteria/test_factory.py
+++ test/criteria/test_factory.py
@@ -1,9 +1,43 @@
 import unittest
+from ddt import ddt, data, unpack
+from app.criteria.factory import CriterionFactory, InvalidCriterionDefinitionError
+from app.criteria.logical import *
+from app.criteria.item import *
+from app.criteria.project import *
 
-from app.criteria.factory import CriteriaFactory
+@ddt
+class TestCriterionFactory(unittest.TestCase):
 
+    def setUp(self):
+        self.criterion_factory = CriterionFactory()
 
-class TestCriteriaFactory(unittest.TestCase):
+    @data( [ { 'true': '' }, TrueCriterion ],
+           [ { 'false': '' }, FalseCriterion ],
+           [ { 'item_has_due_date': '' }, ItemHasDueDateCriterion ])
+    @unpack
+    def test_given_simple_definition_constructs_criterion(self, definition, klass):
+        criterion = self.criterion_factory.create(definition)
+        self.assertIsInstance(criterion, klass)
 
-    def test_true(self):
-        self.assertTrue(False)
+    def test_given_definition_with_multiple_keys_raises_error(self):
+        with self.assertRaises(InvalidCriterionDefinitionError) as ctx:
+            self.criterion_factory.create({ 'true': '', 'false': '' })
+
+        self.assertEquals(str(ctx.exception), 'Definition should only have a single key')
+
+    def test_given_nonexistent_criterion_raises_error(self):
+        with self.assertRaises(InvalidCriterionDefinitionError) as ctx:
+            self.criterion_factory.create({ 'non-existent': '' })
+
+        self.assertEquals(str(ctx.exception), 'No such criterion: non-existent')
+
+    # @data([
+    #     { 'project_name_equals': 'Test' },
+    #     ProjectNameEqualsCriterion,
+    #     { 'project_name': 'Test' }
+    #     ])
+    # @unpack
+    # def test_given_arguments_constructs_criterion(self, definition, klass, state):
+    #     criterion = self.criterion_factory.create(definition)
+    #     self.assertIsInstance(criterion, klass)
+    #     self.assertEquals(criterion.__dict__, state)
