2eaa3e6fc214f988e7c552502fb0d6bd1440ef35
Daniel Szpisjak <felin.arch@gmail.com>
2016-03-06 20:59:40 +0100

    fixing styling

--- test/criteria/test_project.py
+++ test/criteria/test_project.py
@@ -1,19 +1,18 @@
 import unittest
 
-from app.criteria.project import (ProjectNameEqualsCriterion,
-                                  ProjectNameStartsWithCriterion)
+from app.criteria.project import *
 from test.mocks import MockItem, MockLabel, MockProject
 
 
 class TestProjectNameEqualsCriterion(unittest.TestCase):
 
     def test_project_name_equals_returns_True(self):
-        p = MockProject('name')
+        p = MockProject(name='name')
         pc = ProjectNameEqualsCriterion('name')
         self.assertTrue(pc.applies_to(p))
 
     def test_project_name_does_not_equal_returns_False(self):
-        p = MockProject('name')
+        p = MockProject(name='name')
         pc = ProjectNameEqualsCriterion('name_not_equal')
         self.assertFalse(pc.applies_to(p))
 
@@ -21,11 +20,11 @@ class TestProjectNameEqualsCriterion(unittest.TestCase):
 class TestProjectNameStartsWithCriterion(unittest.TestCase):
 
     def test_project_name_starts_with_returns_True(self):
-        p = MockProject('name is something nice')
+        p = MockProject(name='name is something nice')
         pc = ProjectNameStartsWithCriterion('name')
         self.assertTrue(pc.applies_to(p))
 
     def test_project_name_does_not_start_with_returns_False(self):
-        p = MockProject('name is something bad')
+        p = MockProject(name='name is something bad')
         pc = ProjectNameStartsWithCriterion('nama')
         self.assertFalse(pc.applies_to(p))
