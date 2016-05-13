7e95f32354ae83f1f69de0fb50209a18dba143f1
Daniel Szpisjak <felin.arch@gmail.com>
2016-05-13 23:33:14 +0200

    refact(name-converter): extract method

--- app/util/__init__.py
+++ app/util/__init__.py
@@ -5,13 +5,17 @@ class NameConverter:
     @staticmethod
     def convert_criterion_name(name):
         name = NameConverter._remove_suffix(name, 'Criterion')
-        return re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
+        return NameConverter.convert_to_snake_case(name)
 
     @staticmethod
     def convert_action_name(name):
         name = NameConverter._remove_suffix(name, 'Action')
-        return re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
+        return NameConverter.convert_to_snake_case(name)
 
     @staticmethod
     def _remove_suffix(name, suffix):
         return re.sub('(.*){0}'.format(suffix), r'\1', name)
+
+    @staticmethod
+    def convert_to_snake_case(name):
+        return re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
